6cfa110e0e1b2ad00e8bd2571b4deb421a7ea826
João Pauilo <samotinho@gmail.com>
2020-02-04 22:06:31 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+import 'expo/build/Expo.fx';
+import registerRootComponent from 'expo/build/launch/registerRootComponent';
+import { activateKeepAwake } from 'expo-keep-awake';
+
+import TaskList from './src/screens/TaskList';
+
+if (__DEV__) {
+  activateKeepAwake();
+}
+
+registerRootComponent(TaskList);
---
+++ src/commonStyles.js
@@ -0,0 +1,8 @@
+import * as Font from 'expo-font';
+
+export default{
+  fontFamily: 'Lato',
+  colors: {
+    secundary: '#fff'
+  }
+}
